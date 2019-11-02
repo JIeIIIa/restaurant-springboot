5f7c4277a54b6988ef4f11b59c993257e10f1259
JIeIIIa <stealth@bigmir.net>
2019-11-03 00:39:11 +0200

    Initial commit

---
+++ src/test/java/moc/mape/onishchenko/restaurantspringboot/RestaurantSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package moc.mape.onishchenko.restaurantspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
