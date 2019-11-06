e2608e6c6b2fbc5b4e660de2737c46fcc61449bc
JIeIIIa <stealth@bigmir.net>
2019-11-06 13:38:13 +0200

    Add queries in the repositories

--- src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/DishRepository.java
+++ src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/DishRepository.java
@@ -2,6 +2,10 @@ package moc.mape.onishchenko.restaurantspringboot.repository;
 
 import moc.mape.onishchenko.restaurantspringboot.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DishRepository extends JpaRepository<Dish, Long> {
+    @Query(value = "delete from Dish d where d.id = :id")
+    void removeById(@Param("id") Long id);
 }
--- src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/IngredientRepository.java
+++ src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/IngredientRepository.java
@@ -4,5 +4,10 @@ import moc.mape.onishchenko.restaurantspringboot.entity.Ingredient;
 import moc.mape.onishchenko.restaurantspringboot.entity.IngredientId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, IngredientId> {
+    List<Ingredient> findByIdProduct(Long idProduct);
+
+    List<Ingredient> findByIdDish(Long idDish);
 }
