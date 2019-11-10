cf0b1351ed358da0f9d91aa6db1d208233054639
JIeIIIa <stealth@bigmir.net>
2019-11-11 01:38:10 +0200

    Add method to UserInfoRepository

--- src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/UserInfoRepository.java
+++ src/main/java/moc/mape/onishchenko/restaurantspringboot/repository/UserInfoRepository.java
@@ -2,6 +2,10 @@ package moc.mape.onishchenko.restaurantspringboot.repository;
 
 import moc.mape.onishchenko.restaurantspringboot.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByLoginUser(@Param("loginUser") String login);
 }
