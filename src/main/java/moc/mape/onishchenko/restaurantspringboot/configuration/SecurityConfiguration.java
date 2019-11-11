eafa15ccac8601d9f9dc6a2ea723241fb183d235
JIeIIIa <stealth@bigmir.net>
2019-11-11 17:57:27 +0200

    Add secure zone for administrators

--- src/main/java/moc/mape/onishchenko/restaurantspringboot/configuration/SecurityConfiguration.java
+++ src/main/java/moc/mape/onishchenko/restaurantspringboot/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package moc.mape.onishchenko.restaurantspringboot.configuration;
 
+import moc.mape.onishchenko.restaurantspringboot.entity.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/index",
                         "/registration")
                 .permitAll()
+                .antMatchers("/admin/**")
+                .hasRole(UserRole.ADMIN.name())
                 .anyRequest().authenticated()
                 .and();
 
